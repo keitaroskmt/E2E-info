9c14e0d580e07f8bffa15e4ba25c5f05f4436632
keitaroskmt <keitaroskmt@gmail.com>
2024-06-25 09:24:59 +0900

    Add forward_sequentially for CL loss.

--- src/models/layer_wise_model.py
+++ src/models/layer_wise_model.py
@@ -163,6 +163,49 @@ class LayerWiseModel(nn.Module):
         assert loss is not None
         return ModelForwardResultSpecifiedLayer(feature=x, loss=loss, layer_index=layer_index)
 
+    def forward_with_loss_aug_sequentially(
+        self, x1: Tensor, x2: Tensor, y: Tensor, layer_index: int = 0
+    ) -> ModelForwardResultSpecifiedLayer:
+        """
+        Forward augmented inputs `x1` and `x2` through the model until the layer specified by `layer_index`.
+        It is used for the sequential layer-wise training.
+
+        Args:
+            x1: Input data.
+            x2: Input data.
+            y: Target label.
+            layer_index: Layer index to be trained. It takes the value from 0 to `self.num_trainable_layers` - 1.
+        Returns:
+            `ModelForwardResultSpecifiedLayer` object containing information for the specified layer.
+        """
+        assert 0 <= layer_index < self.num_trainable_layers
+
+        x = torch.cat([x1, x2], dim=0)
+        batch_size = y.size(0)
+        model_input = x.clone()
+        loss = None
+        num_layers = 0
+        for layer in self.layers:
+            if isinstance(layer, TrainableBlock1d) or isinstance(layer, TrainableBlock2d):
+                if layer_index == num_layers:
+                    layer_forward_result: LayerForwardResult = layer.forward_with_loss(x, y, model_input)
+                    loss = layer_forward_result.loss
+                    x = layer_forward_result.output.detach()
+                else:
+                    x = layer(x).detach()
+                num_layers += 1
+            else:
+                x = layer(x).detach()
+            if num_layers > layer_index:
+                break
+        x1, _ = torch.split(x, [batch_size, batch_size], dim=0)
+        if layer_index == self.num_trainable_layers - 1:
+            assert layer_index == num_layers
+            loss = self.criterion(self.classifier(x1), y)
+
+        assert loss is not None
+        return ModelForwardResultSpecifiedLayer(feature=x, loss=loss, layer_index=layer_index)
+
     @property
     def encoder(self) -> nn.Module:
         layers = self.layers
